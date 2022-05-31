6569c296d44de0dec14376ff91c9fc555f1786e1
Nishchay Gajera <nishchaygajera7@gmail.com>
2022-05-31 16:41:03 +0530

    31-5-22 commit

---
+++ js/txt.js
@@ -0,0 +1,41 @@
+//30-5-22
+// var a = 10;
+// var b = 20;
+// console.log(10+10)
+// console.log(20+30)
+// console.log(30*'40')
+// console.log(10 +"10")
+// console.log(a<b)
+// console.log(b<a)
+// console.log(true+true)
+// console.log(true/false)
+// console.log(a+'12')
+
+// 31-5-22
+// var a = 10;
+// {
+//     var a = 20;
+//     console.log(a);
+// }
+// console.log(a);
+
+// let a = 10;
+// {
+//     let a = 20;
+//     console.log(a);
+// }
+// console.log(a)
+
+// const a = 40;
+// {
+//     const a = 50;
+//     console.log(a);
+// }
+// console.log(a);
+
+// var a = 40;
+// {
+//     let a = 50;
+//     console.log(a);
+// }
+// console.log(a);
